c5c4c8ca8730cfd30d1a7f1898ec333655d81365
JRmolina2020 <31552404+JRmolina2020@users.noreply.github.com>
2025-04-25 20:41:54 -0500

    first commit6

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,8 +11,7 @@
     </nav>
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <a href="index3.html" class="brand-link">
-        <img src="img/iconf.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-
+        <span class="brand-text font-weight-light">CUARENTAF</span>
       </a>
 
       <div class="sidebar">
